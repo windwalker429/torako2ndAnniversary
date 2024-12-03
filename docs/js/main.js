64f0b746da4ba4ae3f154a3d724b5671820ff8e1
windwalker <windwalker429@gmail.com>
2024-12-03 19:29:04 +0800

    change event time

--- docs/js/main.js
+++ docs/js/main.js
@@ -252,7 +252,7 @@
 	
 
 	// Set the date we're counting down to
-		var countDownDate = new Date("Dec 10, 2024 20:00:00 GMT+9:00").getTime();
+		var countDownDate = new Date("Dec 10, 2024 20:00:00 GMT+8:00").getTime();
 
 		// Update the count down every 1 second
 		var x = setInterval(function() {
